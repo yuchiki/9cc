5d9fe1e51abfaac61ad49e34492d7b546f6ba2a0
Yuki Imai <yuki08281993@gmail.com>
2019-04-21 20:41:10 +0900

    [Add] add a compiler that compiles an integer.

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+try() {
+    expected="$1"
+    input="$2"
+
+    ./9cc "$input" > tmp.s
+    gcc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+try 40 40
+try 0 0
+
+echo OK
