fe0fed0f68a983f1907fe01faee1d8af504a9625
Yuki Imai <yuki08281993@gmail.com>
2019-04-30 19:31:30 +0900

    format

--- 9cc.c
+++ 9cc.c
@@ -24,8 +24,8 @@ Vector *new_vector() {
 
 void vec_push(Vector *vec, void *elem) {
     if (vec->capacity == vec->len) {
-        vec -> capacity *= 2;
-        vec -> data = realloc(vec->data, sizeof(void *) * vec->capacity);
+        vec->capacity *= 2;
+        vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
     }
     vec->data[vec->len++] = elem;
 }
