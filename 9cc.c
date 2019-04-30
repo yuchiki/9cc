f2fd3d51f20b315cc27ee2a70676c49e3ce4c703
Yuki Imai <yuki08281993@gmail.com>
2019-04-30 17:21:52 +0900

    add unary + and -

--- 9cc.c
+++ 9cc.c
@@ -86,14 +86,20 @@ Node *term() {
     error("開き括弧でも数値でもないトークンです: %s\n", tokens[pos].input);
 }
 
+Node *unary() {
+    if (consume('+')) return term();
+    if (consume('-')) return new_node('-', new_node_num(0), term());
+    return term();
+}
+
 Node *mul() {
-    Node *node = term();
+    Node *node = unary();
 
     for(;;) {
         if (consume('*')) {
-            node = new_node('*', node, term());
+            node = new_node('*', node, unary());
         } else if (consume('/')) {
-            node = new_node('/', node, term());
+            node = new_node('/', node, unary());
         } else {
             return node;
         }
