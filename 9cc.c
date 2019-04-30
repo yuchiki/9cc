eeedb764482b2ea64925d699558b756199493955
Yuki Imai <yuki08281993@gmail.com>
2019-04-30 18:48:50 +0900

    add vector

--- 9cc.c
+++ 9cc.c
@@ -6,6 +6,55 @@
 
 #define MAX_TOKENS 100
 
+//////////////////////// vector
+
+typedef struct {
+    void **data;
+    int capacity;
+    int len;
+} Vector;
+
+Vector *new_vector() {
+    Vector *vec = malloc(sizeof(Vector));
+    vec->data = malloc(sizeof(void *) * 16);
+    vec->capacity = 16;
+    vec->len = 0;
+    return vec;
+}
+
+void vec_push(Vector *vec, void *elem) {
+    if (vec->capacity == vec->len) {
+        vec -> capacity *= 2;
+        vec -> data = realloc(vec->data, sizeof(void *) * vec->capacity);
+    }
+    vec->data[vec->len++] = elem;
+}
+
+
+////////////////////////// vector test
+
+void expect(int line, int expected, int actual) {
+    if (expected == actual) return ;
+    fprintf(stderr, "%d: %d expected, but got %d\n", line, expected, actual);
+    exit(1);
+}
+
+void runtest() {
+    Vector *vec =new_vector();
+    expect(__LINE__, 0, vec->len);
+
+    for(int i = 0; i < 100; i++) vec_push(vec, (void *)i);
+
+    expect(__LINE__, 100, vec->len);
+    expect(__LINE__, 0, (long)vec->data[0]);
+    expect(__LINE__, 50, (long)vec->data[50]);
+    expect(__LINE__, 99, (long)vec->data[99]);
+
+    printf("OK\n");
+}
+
+////////////////////////// util
+
 void error(char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
@@ -308,6 +357,12 @@ int main(int argc, char **argv) {
         fprintf(stderr, "引数の数が不正です\n");
         return 1;
     }
+
+    if (strncmp(argv[1], "-test", 5) == 0) {
+        runtest();
+        return 0;
+    }
+
 
     tokenize(argv[1]);
     Node *node = expr();
