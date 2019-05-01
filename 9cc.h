6e9173d7e9a2e993d5d95e2dcdf4e6be269c643c
Yuki Imai <yuki08281993@gmail.com>
2019-05-01 13:27:57 +0900

    move lexer to lexer.c

--- 9cc.h
+++ 9cc.h
@@ -15,3 +15,25 @@ void vec_push(Vector *vec, void *elem);
 ////////////////////////// vector test
 
 void runtest();
+
+////////////////////////// token
+
+// type of token
+enum {
+    TK_NUM = 256,
+    TK_EQ,
+    TK_NE,
+    TK_LE,
+    TK_GE,
+    TK_LT,
+    TK_GT,
+    TK_EOF
+};
+
+typedef struct Token {
+    int ty;      // type of token
+    int val;
+    char* input; // (for error message)
+} Token;
+
+Vector *tokenize(char *p);
--- main.c
+++ main.c
@@ -17,39 +17,6 @@ void error(char* fmt, ...) {
 
 ///////////////////// tokens
 
-// type of token
-enum {
-    TK_NUM = 256,
-    TK_EQ,
-    TK_NE,
-    TK_LE,
-    TK_GE,
-    TK_LT,
-    TK_GT,
-    TK_EOF
-};
-
-typedef struct Token {
-    int ty;      // type of token
-    int val;
-    char* input; // (for error message)
-} Token;
-
-Token *new_token(int ty, char* input){
-    Token *token = malloc(sizeof(Token));
-    token->ty = ty;
-    token->input = input;
-    return token;
-}
-
-Token *new_token_num(int ty, int val, char* input) {
-    Token *token = malloc(sizeof(Token));
-    token->ty = ty;
-    token->val = val;
-    token->input = input;
-    return token;
-}
-
 Vector *tokens;// tokenized tokens
 int pos = 0;
 
@@ -236,71 +203,6 @@ void gen(Node *node) {
     }
 
     printf(" push rax\n");
-}
-
-Vector *tokenize(char *p) {
-    Vector *tokens = new_vector();
-    while(*p) {
-        // skip spaces
-        if (isspace(*p)) {
-            p++;
-            continue;
-        }
-
-        if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
-            vec_push(tokens, new_token(*p, p));
-            p++;
-            continue;
-        }
-
-        if(strncmp(p,">=",2) == 0) {
-            vec_push(tokens, new_token(TK_GE, p));
-            p+= 2;
-            continue;
-        }
-
-        if(strncmp(p,">",1) == 0) {
-            vec_push(tokens, new_token(TK_GT, p));
-            p+= 1;
-            continue;
-        }
-
-        if(strncmp(p,"<=",2) == 0) {
-            vec_push(tokens, new_token(TK_LE, p));
-            p+= 2;
-            continue;
-        }
-
-        if(strncmp(p,"<",1) == 0) {
-            vec_push(tokens, new_token(TK_LT, p));
-            p+= 1;
-            continue;
-        }
-
-        if(strncmp(p,"==",2) == 0) {
-            vec_push(tokens, new_token(TK_EQ, p));
-            p+= 2;
-            continue;
-        }
-
-        if(strncmp(p,"!=",2) == 0) {
-            vec_push(tokens, new_token(TK_NE, p));
-            p+= 2;
-            continue;
-        }
-
-        if(isdigit(*p)) {
-            char* input = p;
-            vec_push(tokens, new_token_num(TK_NUM, strtol(p, &p, 10), input));
-            continue;
-        }
-
-        error("トークナイズできません。: %s", p);
-        exit(1);
-    }
-
-    vec_push(tokens, new_token(TK_EOF, p));
-    return tokens;
 }
 
 int main(int argc, char **argv) {
